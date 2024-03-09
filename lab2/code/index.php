5181dc750bd75af410a191525d3e65b915672042
Andrey <113201143+Lanceando@users.noreply.github.com>
2024-03-09 20:51:05 +0200

    add Link access (1 task)

--- lab2/code/index.php
+++ lab2/code/index.php
@@ -1,2 +1,16 @@
 <?php
-echo phpinfo();
+
+// 1. Доступн по ссылке
+
+/* Imagine a lot of code here */
+$very_bad_unclear_name = "15 chicken wings";
+
+
+// Write your code here:
+$order =&  $very_bad_unclear_name;
+$order .= " and two number 9s, a number 9 large, a number 6 with extra dip, a number 7, two number 45s, one with cheese, and a large soda.";
+
+
+
+// Don't change the line below
+echo "\nYour order is: $very_bad_unclear_name.";
