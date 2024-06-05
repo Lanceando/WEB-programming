b4436c58cb9696e76152bd72025b3226801e437b
Andrey <113201143+Lanceando@users.noreply.github.com>
2024-06-05 02:38:18 +0200

    complete index.php, add save.php

--- lab4/code/index.php
+++ lab4/code/index.php
@@ -1,2 +1,72 @@
-<?php
-echo phpinfo();
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>lab 4</title>
+</head>
+<body>
+<div id="form">
+    <form action="save.php" method="post">
+        <label for="email">Email</label>
+        <input type="email" name="email" required>
+
+        <label for="category">Category</label>
+        <select name="category" required>
+            <option value="electronics">Electronics</option>
+            <option value="clothing">Clothing</option>
+            <option value="books">Books</option>
+            <option value="furniture">Furniture</option>
+            <option value="other">Other</option>
+        </select>
+
+        <label for="title">Title</label>
+        <input type="text" name="title" required>
+
+        <label for="description">Description</label>
+        <textarea rows="5" name="description"></textarea>
+
+        <input type="submit" value="Save">
+    </form>
+
+</div>
+
+<div id="table">
+    <table>
+        <thead>
+        <tr>
+            <th>Category</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Email</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        // Чтение данных из Google Sheets
+        $id = '1meHQWbVqZ0nv8mZy501P5L858hFYkScPDYmmUpj7fcA';
+        $gid = 0;
+        $csv = file_get_contents('https://docs.google.com/spreadsheets/d/' . $id . '/export?format=csv&gid=' . $gid);
+        $csv = explode("\r\n", $csv);
+        $array = array_map('str_getcsv', $csv);
+
+        // Вывод объявлений на сайте
+        $html = '';
+        foreach ($array as $row) {
+            $html .= '<tr>';
+            $html .= '<td>' . (isset($row[0]) ? $row[0] : '') . '</td>';
+            $html .= '<td>' . (isset($row[1]) ? $row[1] : '') . '</td>';
+            $html .= '<td>' . (isset($row[2]) ? $row[2] : '') . '</td>';
+            $html .= '<td>' . (isset($row[3]) ? $row[3] : '') . '</td>';
+            $html .= '</tr>';
+        }
+
+        echo $html;
+        ?>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
---
+++ lab4/code/save.php
@@ -0,0 +1,43 @@
+<?php
+
+function redirectToHome(): void
+{
+    header('Location: /');
+    exit();
+}
+
+$URL_APP = "https://script.google.com/macros/s/AKfycbzqqqn0xwDxhMagGpyXQnCwdGpfBxja7a6FpeQEqMDeCAeCuv1-TpyJVKdzje6HxZMzkw/exec";
+
+function is_filled(array $details): bool
+{
+    return !empty($details['title']) && !empty($details['email']) && !empty($details['category']) && !empty($details['description']);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $title = $_POST['title'];
+    $email = $_POST['email'];
+    $category = $_POST['category'];
+    $description = $_POST['description'];
+
+    $details = [
+        'title' => $title,
+        'email' => $email,
+        'category' => $category,
+        'description' => $description,
+    ];
+
+    if (!is_filled($details)) {
+        exit('Поля формы не заполнены');
+    }
+
+    $formBody = http_build_query($details, '', '&');
+
+    $response = json_decode(file_get_contents($URL_APP, false, stream_context_create([
+        'http' => [
+            'method'  => 'POST',
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'content' => $formBody,
+        ],
+    ])), true);
+}
+redirectToHome();
